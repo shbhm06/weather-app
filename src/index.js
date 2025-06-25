a1554102cdba9efb859788bd915bf6612ee71e9d
shbhm06 <shubham.shreekumar@gmail.com>
2025-06-26 00:42:17 +0400

    Added logic for manipulating DOM for adding data on page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+import backgroundImage from './images/background.jpg';
+import './styles.css';
+document.documentElement.style.setProperty('--bg-image', `url(${backgroundImage})`);
+
 async function getData (place) {
     console.log('getData called with:', place);
     const response = await fetch (`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?key=B9PW4AEAY8QPFK7BS9D54QCHA`, {mode: 'cors'});
+    
+    if(!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
     const placeData = await response.json();
-    const placeDataDew = await placeData.currentConditions.dew;
     return placeData;
 }
 
@@ -10,6 +18,30 @@ function processWeatherData(rawData) {
 
     const current = rawData.currentConditions;
     const today = rawData.days[0];
+
+    const hourly24 = today.hours.map(hour => {
+        const time24 = hour.datetime;
+        const hourNum = parseInt(time24.split(':')[0]);
+
+        let displayTime
+        if(hourNum === 0) displayTime = '12AM';
+        else if (hourNum === 12) displayTime = '12PM';
+        else if (hourNum < 12) displayTime = `${hourNum}AM`;
+        else displayTime = `${hourNum - 12}PM`;
+
+        return {
+            time: displayTime,
+            temperature: Math.round(hour.temp)
+        };
+    });
+
+    const sixDayForecast = rawData.days.slice(0,6).map(day => {
+        return {
+            date: day.datetime,
+            highTemp: Math.round(day.tempmax),
+            lowTemp: Math.round(day.tempmin)
+        };
+    });
 
     return {
         location: rawData.resolvedAddress,
@@ -19,8 +51,10 @@ function processWeatherData(rawData) {
         humidity: current.humidity,
         visibility: current.visibility,
         feelsLike: current.feelslike,
-        sunrise: current.sunrise,
-        sunset: current.sunset,
+        sunrise: today.sunrise,
+        sunset: today.sunset,
+        hourlyForecast: hourly24,
+        sixDayForecast: sixDayForecast
     }
 }
 
@@ -34,6 +68,123 @@ async function getWeatherData(place) {
     }
 }
 
-getWeatherData('London').then(data => {
-    console.log('Proccessed data: ', data);
+
+
+const searchContent = document.getElementById("search-content");
+const searchBtn = document.getElementById("searchBtn");
+
+searchBtn.addEventListener("click", () => {
+
+    let searchVal = searchContent.value;
+
+    if(!searchVal) {
+        console.log('enter the name of the place!');
+    }
+
+    getWeatherData(searchVal).then(data => {
+        console.log('Proccessed data: ', data);
+        displayData(data);
+        overviewText(data);
+        forecastText(data);
+    });
 });
+
+const placeName = document.getElementById('place-name');
+const placeTemp = document.getElementById('place-temp');
+const placeRain = document.getElementById('place-rain');
+
+const uvIndex = document.querySelector('#uv-index .bottom-data');
+const humidity = document.querySelector('#humidity .bottom-data');
+const visibility = document.querySelector('#visibility .bottom-data');
+const feelsLike = document.querySelector('#feels-like .bottom-data');
+const sunrise = document.querySelector('#sunrise .bottom-data');
+const sunset = document.querySelector('#sunset .bottom-data');
+
+function displayData(data) {
+
+    const nameText = data.location;
+    placeName.textContent = nameText;
+
+    const tempText = data.temperature;
+    placeTemp.textContent = `${tempText}°F`;
+
+    const rainText = data.chanceOfRain;
+    placeRain.textContent = `Chance of Rain: ${rainText}%`;
+
+    const uvText = data.uvIndex;
+    uvIndex.textContent = uvText;
+
+    const humidText = data.humidity;
+    humidity.textContent = `${humidText}%`;
+
+    const visText = data.visibility;
+    visibility.textContent = `${visText} mi.`;
+
+    const feelsText = data.feelsLike;
+    feelsLike.textContent = `${feelsText}°F`;
+
+    const riseText = data.sunrise;
+    sunrise.textContent = riseText;
+
+    const setText = data.sunset;
+    sunset.textContent = setText;
+}
+
+function overviewText(data) {
+    
+    const overviewContainer = document.getElementById('overview-container');
+    overviewContainer.innerHTML = '';
+    
+    
+    for (let i = 0; i < 24; i++) {
+        const hour = data.hourlyForecast[i];
+        
+        const card = document.createElement('div');
+        card.className = 'overview-card';
+        
+        const cardTime = document.createElement('div');
+        cardTime.textContent = hour.time;
+        
+        const cardTemp = document.createElement('div');
+        cardTemp.textContent = `${hour.temperature}°F`;
+        
+        card.appendChild(cardTime);
+        card.appendChild(cardTemp);
+        
+        overviewContainer.appendChild(card);
+    }
+}
+
+function forecastText(data) {
+
+    const weekCardContainer = document.getElementById('week-card-container');
+    weekCardContainer.innerHTML='';
+
+    const dayLabels = [
+        'Tommorow',
+        '2 days from now',
+        '3 days from now',
+        '4 days from now',
+        '5 days from now',
+    ];
+
+    for(let i=1; i <= 5; i++) {
+        const dayData = data.sixDayForecast[i];
+
+        const forecastCard = document.createElement('div');
+        forecastCard.className = 'forecast-card';
+
+        const dayLabel = document.createElement('div');
+        dayLabel.className = `day-${i}-forecast`;
+        dayLabel.textContent = dayLabels[i - 1];
+
+        const tempDisplay = document.createElement('div');
+        tempDisplay.className = 'temp-forcast';
+        tempDisplay.textContent = `High/Low: ${dayData.highTemp}°/${dayData.lowTemp}°`;
+
+        forecastCard.appendChild(dayLabel);
+        forecastCard.appendChild(tempDisplay);
+
+        weekCardContainer.appendChild(forecastCard);
+    }
+}
